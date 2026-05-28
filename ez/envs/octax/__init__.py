d647d4b189978249001d75563f9e61cbdb37cd7a
buzhaoliu <buzhao@wustl.edu>
2026-05-27 23:34:22 -0500

    fix obs shape

---
+++ ez/envs/octax/__init__.py
@@ -0,0 +1,18 @@
+from ez.envs.base import BaseWrapper
+
+
+class OctaxWrapper(BaseWrapper):
+    """Convert Octax's Gymnasium-style API to EZv2's classic Gym API."""
+
+    def __init__(self, env, obs_to_string=False):
+        super().__init__(env, obs_to_string, False)
+
+    def reset(self, **kwargs):
+        obs, info = self.env.reset(**kwargs)
+        return self.format_obs(obs)
+
+    def step(self, action):
+        obs, reward, terminated, truncated, info = self.env.step(action)
+        obs = self.format_obs(obs)
+        info["raw_reward"] = reward
+        return obs, reward, terminated or truncated, info
