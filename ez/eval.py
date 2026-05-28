65539e3fe8c9599c683e33c52ee67903c212a207
buzhaoliu <buzhao@wustl.edu>
2026-05-28 10:27:40 -0500

    fix bug in workers

--- ez/eval.py
+++ ez/eval.py
@@ -145,8 +145,10 @@ def eval(agent, model, n_episodes, save_path, config, max_steps=None, use_pb=Fal
             # save data to trajectory buffer
             game_trajs[i].store_search_results(values[i], r_values[i], r_policies[i])
             game_trajs[i].append(action, obs, reward)
-            if config.env.env in ['Atari', 'Octax']:
+            if config.env.env == 'Atari':
                 game_trajs[i].snapshot_lst.append(envs[i].ale.cloneState())
+            elif config.env.env == 'Octax':
+                game_trajs[i].snapshot_lst.append([])
             else:
                 game_trajs[i].snapshot_lst.append(envs[i].physics.get_state())
 
--- ez/worker/batch_worker.py
+++ ez/worker/batch_worker.py
@@ -890,12 +890,12 @@ class BatchWorker(Worker):
         for i in range(length):
             if policy == 'search':
                 tree = mcts.names[self.config.mcts.language](
-                    num_actions=self.config.env.action_space_size if self.env == 'Atari' else self.config.mcts.num_top_actions,
+                    num_actions=self.config.env.action_space_size if self.env in ['Atari', 'Octax'] else self.config.mcts.num_top_actions,
                     discount=self.config.rl.discount,
                     **self.config.mcts,  # pass mcts related params
                     **self.config.model,  # pass the value and reward support params
                 )
-                if self.env == 'Atari':
+                if self.env in ['Atari', 'Octax']:
                     if self.config.mcts.use_gumbel:
                         r_values, r_policies, best_actions, _ = tree.search(
                             self.model, len(states), states, values, policies,
@@ -915,7 +915,7 @@ class BatchWorker(Worker):
             if policy == 'search':
                 actions = torch.from_numpy(np.asarray(best_actions)).cuda().float()
             else:
-                if self.env == 'Atari':
+                if self.env in ['Atari', 'Octax']:
                     actions = F.gumbel_softmax(policies, hard=True, dim=-1, tau=1e-4)
                     actions = actions.argmax(dim=-1)
                 else:
